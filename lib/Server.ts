da7d16751c4b3ff263ab03807a1cba118f9a5cf2
Alex Hicks <alex@alexhicks.net>
2018-03-26 07:18:04 -0400

    Implement ridiculously simple beginning

---
+++ lib/Server.ts
@@ -0,0 +1,37 @@
+import * as _ from "lodash";
+import * as events from "events";
+import * as express from "express";
+import * as http from "http";
+import ServerOptions from "./options/ServerOptions";
+
+export default class Server extends events.EventEmitter {
+    public server: http.Server;
+    public express: express.Application;
+    public options: ServerOptions;
+
+    public constructor(options: ServerOptions | undefined) {
+        super();
+        this.options = _.defaults<ServerOptions, ServerOptions>(options, {
+            port: 3000,
+            hostname: undefined
+        });
+    }
+
+    public start(): Promise<void> {
+        // don't want to start server if it's already started
+        if (this.express !== undefined) return Promise.resolve();
+        this.express = express();
+        this.server = http.createServer(this.express);
+        return new Promise<void>((resolve, reject) => {
+            this.server.listen(this.options.port, this.options.hostname, resolve);
+        });
+    }
+
+    public stop(): Promise<void> {
+        // can't stop it if it isn't started
+        if (this.express === undefined) return Promise.resolve();
+        return new Promise<void>((resolve, reject) => {
+            this.server.close(resolve);
+        });
+    }
+}
