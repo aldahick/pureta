75bf0f58a00bbfea22f4ab2f894f9b28fb025cd8
Alex Hicks <alex@alexhicks.net>
2018-03-28 15:54:19 -0400

    Fix types for promise-events

--- def/promise-events.d.ts
+++ def/promise-events.d.ts
@@ -2,14 +2,14 @@ declare module "promise-events" {
     import * as events from "events";
 
     export class EventEmitter extends events.EventEmitter {
-        addListener(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
+        addListener<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
         emit<T>(eventName: string, ...args: any[]): Promise<T[]>;
-        on(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
-        once(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
-        prependListener(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
-        prependOnceListener(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
+        on<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
+        once<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
+        prependListener<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
+        prependOnceListener<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
         removeAllListeners(eventName: string | undefined): Promise<void>;
-        removeListener(eventName: string, listener: (...args: any[]) => Promise<void>): Promise<void>;
+        removeListener<T>(eventName: string, listener: (...args: any[]) => Promise<T>): Promise<void>;
         // have to have these to override default EventEmitter methods
         // https://stackoverflow.com/a/39923895
         addListener(): never;
